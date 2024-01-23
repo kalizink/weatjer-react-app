578495bc4b02851ecbcbdfe23db6b80d32e5e2e0
kalizink <kalizink65@gmail.com>
2024-01-23 12:27:37 -0600

    Added basic styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Footer from "./Footer.js";
+import Weather from "./Weather.js";
 
 export default function App() {
 	return (
 		<div className="App">
 			<div className="container">
+				<Weather />
 				<Footer />
 			</div>
 		</div>
